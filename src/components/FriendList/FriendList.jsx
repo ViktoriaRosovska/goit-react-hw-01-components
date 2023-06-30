334d8f36309a80ea2c4921995a0bd8fa948f1753
Viktoria Rosovska <torimeister@gmail.com>
2023-06-30 20:16:05 +0300

    add status online 3 task

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,13 +1,13 @@
-
+import clsx from 'clsx';
 import css from './FriendList.module.css';
 import friends from 'data/friends.json';
 
 const FriendListItem = ({friendList}) => {
     return (
-            friendList.map(({ name, avatar, isOnline, id }) => {
+            friendList.map(({ name, avatar, isOnline, id }) => { 
         return (
-            <li className={css.item} key={id}>
-            <span className={ css.status }></span>
+            <li className={css.item } key={id}>    
+            <span className={clsx(css.status, isOnline ? css.online : css.ofline)} ></span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
             </li>
