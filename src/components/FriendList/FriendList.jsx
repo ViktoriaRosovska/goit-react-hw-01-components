efeaca06dc057ff77e42e1279dc77f797bfd7a81
Viktoria Rosovska <torimeister@gmail.com>
2023-07-01 19:26:08 +0300

    add proptypes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,19 @@
 import { FriendListItem } from './FriendListItem';
-
+import PropTypes from 'prop-types';
 export const FriendList = ({ friends }) => {
-    return (
-        <ul className="friendList">
-            {friends.map(friend => 
-             (<FriendListItem friend={friend} key={friend.id} />)
-            )}
-        </ul>
-    )
-}
+  return (
+    <ul className="friendList">
+      {friends.map(friend => (
+        <FriendListItem friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.object({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,10 +1,18 @@
 import clsx from 'clsx';
 import css from './FriendList.module.css';
-
+import PropTypes from 'prop-types';
 export const FriendListItem = ({ friend: { name, avatar, isOnline } }) => (
-        <li className={css.item}>
-        <span className={clsx(css.status, isOnline ? css.online : css.ofline)} ></span>
-            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-            <p className={css.name}>{name}</p>
-        </li>
-    )
+  <li className={css.item}>
+    <span
+      className={clsx(css.status, isOnline ? css.online : css.ofline)}
+    ></span>
+    <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+    <p className={css.name}>{name}</p>
+  </li>
+);
+
+FriendListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+};
